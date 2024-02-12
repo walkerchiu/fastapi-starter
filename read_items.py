fbef0f2ce2e6e07c4bf51fc65d3cf28f41a7eba7
walkerchiu <chenjen.chiou@gmail.com>
2024-02-12 08:04:44 +0800

    Feat(API): Add constraints for query parameter length and pattern in read_items api
    Reference:
    1. https://fastapi.tiangolo.com/tutorial/query-params-str-validations/#add-regular-expressions.

--- read_items.py
+++ read_items.py
@@ -5,22 +5,14 @@ from fastapi import FastAPI, Query
 app = FastAPI()
 
 
-# Define a route to read items with an optional query parameter
-# https://fastapi.tiangolo.com/tutorial/query-params-str-validations/#additional-validation
+# Define a route to read items with an optional query parameter that is constrained by length and pattern
+# https://fastapi.tiangolo.com/tutorial/query-params-str-validations/#add-regular-expressions.
 @app.get("/items/")
-async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
-    """
-    The query parameter q is of type Annotated[str | None, Query(max_length=50)].
-    Annotated is used to add metadata to the type hint. Here, it indicates that the query parameter
-    should be of type str or None, with a maximum length of 50 characters.
-
-    If the q parameter is provided and its length exceeds 50 characters, FastAPI will raise an error.
-
-    The default value is None, making the parameter optional.
-
-    Using Annotated provides additional type information and validation to the endpoint.
-    """
-
+async def read_items(
+    q: Annotated[
+        str | None, Query(min_length=3, max_length=50, pattern="^fixedquery$")
+    ] = None,
+):
     # Initialize results with a list of items
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     # If a query parameter q is provided, add it to the results dictionary
