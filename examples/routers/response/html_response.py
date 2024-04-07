f96f85fe424e27efb310ef56cb27f8c9afc9082f
walkerchiu <chenjen.chiou@gmail.com>
2024-04-07 08:23:20 +0800

    Feat(API): Add read_items2 API to demonstrate another way to use HTMLResponse
    Reference:
    1. https://fastapi.tiangolo.com/advanced/custom-response/#return-a-response.

--- examples/routers/response/html_response.py
+++ examples/routers/response/html_response.py
@@ -5,6 +5,7 @@ router = APIRouter()
 
 
 # Define a route that returns an HTML response using HTMLResponse
+# This approach directly returns an HTML string.
 # https://fastapi.tiangolo.com/advanced/custom-response/#html-response.
 @router.get("/items/", response_class=HTMLResponse)
 async def read_items():
@@ -19,3 +20,23 @@ async def read_items():
         </body>
     </html>
     """
+
+
+# Define another route that returns an HTML response using HTMLResponse
+# This approach explicitly creates an HTMLResponse object and returns it.
+# https://fastapi.tiangolo.com/advanced/custom-response/#return-a-response.
+@router.get("/items2/")
+async def read_items2():
+    # Create an HTML content string
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    # Return an HTML response object with the specified content and status code
+    return HTMLResponse(content=html_content, status_code=200)
