cb71f95d32a3d5f3906e906949be6872261de546
walkerchiu <chenjen.chiou@gmail.com>
2024-04-14 15:17:53 +0800

    Feat(API): Add streaming_large_video API to demonstrate how to stream a large video file

--- examples/routers/response/streaming.py
+++ examples/routers/response/streaming.py
@@ -16,3 +16,28 @@ async def fake_video_streamer():
 @router.get("/")
 async def streaming():
     return StreamingResponse(fake_video_streamer())
+
+
+# Define a route that streams a large video file.
+# If you have a file-like object (e.g. the object returned by open()),
+# you can create a generator function to iterate over that file-like object.
+# https://fastapi.tiangolo.com/advanced/custom-response/#streamingresponse.
+@router.get("/large-video")
+async def streaming_large_video():
+    some_file_path = "large-video-file.mp4"
+
+    # Define a generator function to iterate over the file-like object.
+    def iterfile():
+        # Open the file-like object in binary read mode.
+        with open(some_file_path, mode="rb") as file_like:
+            # Yield chunks of the file as they are read.
+            yield from file_like  #
+
+    return StreamingResponse(iterfile(), media_type="video/mp4")
+
+
+# That way, you don't have to read it all first in memory,
+# and you can pass that generator function to the StreamingResponse, and return it.
+
+# Notice that here as we are using standard open() that doesn't support async and await,
+# we declare the path operation with normal def.
